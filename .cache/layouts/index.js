c739ede6a4b45c853afeed20f32c0f793d903cb0
macs91 <massimodemarchi91@gmail.com>
2018-02-27 22:24:36 +0000

    remove cache folder

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/home/macs/repos/abit.company/src/layouts/index.js"
-  import data from "/home/macs/repos/abit.company/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
