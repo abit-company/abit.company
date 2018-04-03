426b1e21f9d8aff4665e7b32ef6879e9d38b5d4a
mmarcon91 <m.marcon.91@gmail.com>
2018-04-03 15:53:07 +0200

    working on service #1

--- src/components/Service.js
+++ src/components/Service.js
@@ -15,16 +15,38 @@ type Props = {
 };
 
 const Service = ({
+  className,
   service: { id, frontmatter: { icon, title: name, description } }
 }: Props) => (
-  <Wrapper>
-    <Icon src={icon} alt={`${name} avatar`} />
+  <Wrapper className={className}>
+    <IconContainer>
+      <Icon src={icon} alt={`${name} icon`} />
+    </IconContainer>
+
+    <TextContainer>
+      <Name>{name}</Name>
+      <Description>{description}</Description>
+    </TextContainer>
   </Wrapper>
 );
 
 const Wrapper = styled.div`
-  padding: 20px 20px 40px 20px;
   box-sizing: border-box;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  text-align: left;
+  &:nth-child(even) {
+    flex-direction: row-reverse;
+    text-align: right;
+  }
+`;
+
+const IconContainer = styled.div`
+  padding: 20px 20px 20px 20px;
+  box-sizing: border-box;
+  flex-basis: 25%;
 `;
 
 const Icon = styled(({ children, className, src, alt }) => (
@@ -41,4 +63,27 @@ const Icon = styled(({ children, className, src, alt }) => (
   }
 `;
 
-export default Service;
+const TextContainer = styled.div`
+  flex-basis: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const Name = styled.p`
+  font-size: 16px;
+  font-weight: 600;
+  color: #0c35fb;
+  width: 100%;
+`;
+
+const Description = styled.p`
+  font-size: 16px;
+  font-weight: 600;
+  color: #000629;
+  padding-top: 20px;
+  width: 100%;
+  line-height: 1.4em;
+`;
+
+export default styled(Service)``;
