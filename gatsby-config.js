50771101c827b99a4e2bfa761c27f64c8a3eedc1
Gianluca Chiap <gianlucachiap@gmail.com>
2019-03-29 09:45:46 +0100

    analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,5 +25,17 @@ module.exports = {
         icon: `src/images/abitcompany-logo.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-117352627-1',
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+      },
+    },
   ],
 };
